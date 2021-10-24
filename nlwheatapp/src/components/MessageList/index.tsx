94c190f9f11ce63db871d7c323bd42bac3ca6fd7
victorjunior95 <victorjr.muchacho@gmail.com>
2021-10-24 00:30:26 -0300

    Stage 3 completed

--- nlwheatapp/src/components/MessageList/index.tsx
+++ nlwheatapp/src/components/MessageList/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
-// import { api } from '../../services/api';
-// import { io } from 'socket.io-client';
+import { api } from '../../services/api';
+import { io } from 'socket.io-client';
 
-// import { MESSAGES_EXAMPLE } from '../../utils/messages';
+import { MESSAGES_EXAMPLE } from '../../utils/messages';
 
 import { Message, MessageProps } from '../Message';
 
@@ -12,49 +12,49 @@ import { styles } from './styles';
 // fila de mensagens
 // let messagesQueue: MessageProps[] = []; // tipagem para variáveis do tipo let
 // Exemplo com MESSAGES_EXAMPLE como valor inicial da fila de mensagens
-// let messagesQueue: MessageProps[] = MESSAGES_EXAMPLE;
+let messagesQueue: MessageProps[] = MESSAGES_EXAMPLE;
 
-// const socket = io(String(api.defaults.baseURL));
-// socket.on('new_message', (newMessage) => {
-//   messagesQueue.push(newMessage);
-// });
+const socket = io(String(api.defaults.baseURL));
+socket.on('new_message', (newMessage) => {
+  messagesQueue.push(newMessage);
+});
 
 export function MessageList(){
-  // const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
-  const message = {
-    id: "1",
-    text: "mensagem de teste",
-    user: {
-      name: "Victor Junior",
-      avatar_url: "https://github.com/victorjunior95.png"
-    }
-  }
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+  // const message = {
+  //   id: "1",
+  //   text: "mensagem de teste",
+  //   user: {
+  //     name: "Victor Junior",
+  //     avatar_url: "https://github.com/victorjunior95.png"
+  //   }
+  // }
 
   // executado 1x trazendo as últimas três mensagens
-  // useEffect(() => {
-  //   async function fetchMessages() {
-  //     const messagesResponse = await api.get<MessageProps[]>('/messages/last3');
-  //     setCurrentMessages(messagesResponse.data);
-  //   }
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>('/messages/last3');
+      setCurrentMessages(messagesResponse.data);
+    }
 
-  //   fetchMessages();
-  // }, []);
+    fetchMessages();
+  }, []);
 
-  // useEffect(() => {
-  //   const THREE_SECONDS = 3000
-  //   const timer = setInterval(() => {
-  //     if (messagesQueue.length > 0) {
-  //       // prevState é uma referência ao estado prévio/anterior
-  //       setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[1]]);
-  //       // remove o primeiro elemento dado que já foi exibido
-  //       messagesQueue.shift();
-  //     }
-  //   }, THREE_SECONDS);
+  useEffect(() => {
+    const THREE_SECONDS = 3000
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        // prevState é uma referência ao estado prévio/anterior
+        setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[1]]);
+        // remove o primeiro elemento dado que já foi exibido
+        messagesQueue.shift();
+      }
+    }, THREE_SECONDS);
 
-  //   // função de desmontagem do useEffect, limpando variáveis/dados da memória
-  //   //    resetar o timer
-  //   return () => clearInterval(timer);
-  // }, []);
+    // função de desmontagem do useEffect, limpando variáveis/dados da memória
+    //    resetar o timer
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <ScrollView
@@ -65,12 +65,12 @@ export function MessageList(){
       keyboardShouldPersistTaps="never"
     >
       {
-        // currentMessages.map(message => <Message key={message.id} data={message} />)
+        currentMessages.map(message => <Message key={message.id} data={message} />)
       }
 
+      {/* <Message data={message} />
       <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+      <Message data={message} /> */}
     </ScrollView>
   );
 }
--- nlwheatapp/src/services/api.ts
+++ nlwheatapp/src/services/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: 'http://172.17.0.1:4000'
-  baseURL: 'http://192.168.1.112:4000'
+  // baseURL: 'http://192.168.1.112:4000'
   // baseURL: 'http://127.0.0.1:4000'
+  baseURL: 'http://b40a-45-163-229-207.ngrok.io'
 });
